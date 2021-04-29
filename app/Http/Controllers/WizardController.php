455467462a327874b1807cc8bb2470897e5c9261
Maheeb <maheeb009@gmail.com>
2021-04-29 19:31:11 +0600

    controller setup done

--- app/Http/Controllers/WizardController.php
+++ app/Http/Controllers/WizardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\UserInformation;
 use Illuminate\Http\Request;
 
 class WizardController extends Controller
@@ -15,21 +16,56 @@ class WizardController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
+//        dd($request->all());
 
-//        $user = new User();
-//
-//        $user->name = $request->name;
-//        $user->email = $request->email;
-//        $user->phone = $request->phone;
-//        $user->NID = $request->NID;
-//
-//        $user->save();
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->nid = $request->nid;
+        if ($request->hasFile('avatar')) {
+            $avatar = time() . '.' . $request->avatar->getClientOriginalExtension();
+            $user->avatar = "images/avatars/{$avatar}";
+            $request->avatar->move(public_path('images/avatars/'), $avatar);
+        }
+        if ($request->hasFile('nid_first_part')) {
+            $nid_first_part = time() . '.' . $request->nid_first_part->getClientOriginalExtension();
+            $user->nid_first_part = "images/nid_first_part/{$nid_first_part}";
+            $request->nid_first_part->move(public_path('images/nid_first_part/'), $nid_first_part);
+        }
+
+        if ($request->hasFile('nid_second_part')) {
+            $nid_second_part = time() . '.' . $request->nid_second_part->getClientOriginalExtension();
+            $user->nid_second_part = "images/nid_second_part/{$nid_second_part}";
+            $request->nid_second_part->move(public_path('images/nid_second_part/'), $nid_second_part);
+        }
+
+
+        $userStore= $user->save();
+
+        if ($userStore){
+
+            $userInformation = new UserInformation();
+
+            $userInformation->user_id = $user->id;
+            $userInformation->educations = $request->educations;
+            $userInformation->experiences = $request->experiences;
+            $userInformation->publications = $request->publications;
+
+            $userInformation->save();
+
+        }
+
+        return response()->json([
+            'user'=>$user
+        ]);
 //        dd($request->jobs, $request->education);
 //        dd($request->jobs);
 //        $data = json_decode($request->jobs, TRUE);
 //        $test = json_encode($data);
 //        dd($request->file('picture'));
     }
+
+
 
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->string('phone');
-            $table->string('NID');
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('nid')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('nid_first_part')->nullable();
+            $table->string('nid_second_part')->nullable();
             $table->timestamps();
         });
     }
